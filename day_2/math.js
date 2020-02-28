55ddb713c29a5e7f073214faca87baa23a86ee5e
Mehdi <semlali.mehdi.1980@gmail.com>
2020-02-28 21:38:43 +0100

    adding day_2 math and strings

---
+++ day_2/math.js
@@ -0,0 +1,41 @@
+const PI = Math.PI;
+
+console.log(PI);    // 3.141592653589793
+
+// Rounding to the closest number
+// if .5 up if 0.5 down rounding 
+
+console.log(Math.round(9.81));           // 10
+console.log(Math.floor(PI));             // 3 rounging down
+console.log(Math.ceil(PI));              // 4 rounding up
+
+console.log(Math.min(-5, 3, 2, 5.3));    // -5
+console.log(Math.max(-5, 3, 2, 5.3));    // 5.3
+
+const randNum = Math.random();
+console.log(randNum);                    // a random num (0.0 to 0.9)
+
+const numFloor = Math.floor(Math.random()*11); // create (0 to 10)
+console.log(numFloor);
+
+
+console.log(Math.abs(-10));         // 10 - absolute value
+
+console.log(Math.sqrt(100));        // 10 - square root
+console.log(Math.sqrt(2));          // 1.4142135623730951 - square root
+
+console.log(Math.pow(3, 2));        // 9
+
+console.log(Math.E);                // 2.718
+
+//--------------------------------------------------------//
+
+console.log("//------Trigonometry------//");
+console.log(Math.sin(0));              // 0
+console.log(Math.sin(60));              // -0.3048106211022167
+
+console.log(Math.cos(0));              // 1
+console.log(Math.cos(60));              // -0.9524129804151563
+
+
+
