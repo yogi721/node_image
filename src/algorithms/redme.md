4d83a947a6c0879f8d2aa2bf0c721ebfe90d7f24
Mehdi <semlali.mehdi.1980@gmail.com>
2020-03-06 21:34:18 +0100

    adding reverse string function

--- src/algorithms/redme.md
+++ src/algorithms/redme.md
@@ -5,3 +5,19 @@
     Or npm:
 
     npm install --save-dev jest
+
+Add the following section to your package.json:
+
+    {
+        "scripts": {
+            "test": "jest"
+        }
+    }
+
+# run test
+
+    npm run test
+
+# Comment/Uncomment
+
+vscode: Ctrl+K+C/Ctrl+K+U
--- src/algorithms/stephGrid/string-reversal/index.js
+++ src/algorithms/stephGrid/string-reversal/index.js
@@ -3,14 +3,28 @@
 // Take the character and add it to the start of 'reversed'
 // Return the variable reversed
 
-function reverse(str){
-    let reversed = '';
+//************************************************** */
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
 
-    for(let character of str){
-        reversed = character + reversed;
-    }
-
-    return reversed;
+//************************************************** */
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
+
+//*************************************************** */
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
 
 module.exports = reverse;
