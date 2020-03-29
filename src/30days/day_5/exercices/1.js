7ec3f464e9f27ea03f87c3c32fdae7e7957f14db
Mehdi <semlali.mehdi.1980@gmail.com>
2020-03-30 00:44:19 +0200

    Adding Data Structures

---
+++ src/30days/day_5/exercices/1.js
@@ -0,0 +1,35 @@
+// Declare an empty array;
+const arr = Array(); // OR arr = new Array() OR arr = []
+console.log(arr);
+
+// Declare an array with more than 5 number of elements
+const arr1 = [1, 2, 3, 4, 5, 6];
+console.log(arr1);
+
+// Find the length of your array
+console.log(arr1.length);
+
+// Get the first item, the middle item and the last item of the array
+console.log(arr1[0]);
+console.log(arr1[3]);
+console.log(arr1[5]);
+
+// Declare an array called mixedDataTypes,put different data types in your array and find length of the array. You are should size be greater than 5
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// Print the array using console.log()
+// Print the number of companies in the array
+// Print the first company, middle and last company
+// Print out each company
+// Change each company name to uppercase one by one and print them out
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// Filter out companies which have more than one 'o' without the filter method
+// Sort the array using sort() method
+// Reverse the array using reverse() method
+// Slice out the first 3 companies from the array
+// Slice out the last 3 companies from the array
+// Slice out the middle IT company or companies from the array
+// Remove the first IT company from the array
+// Remove the middle IT company or companies from the array
+// Remove the last IT company from the array
+// Remove all IT companies
---
+++ src/30days/day_5/exercices/2.js
@@ -0,0 +1,10 @@
+// Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js ans store the webTechs array in to this file. Access both file in main.js file
+
+// First remove all the functions and change the string to array
+// and count the number of words in the array
+let text =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+
+let words = text.replace(',', '');
+console.log(words);
+console.log(words.length);
